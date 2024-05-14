414f42d52fbb926b070bd96c2e01d8edb252f8d9
Vitaly Senokosov <6545415@gmail.com>
2024-05-14 10:53:31 +0300

    add /hostname endpoint

--- hw02_docker/main.go
+++ hw02_docker/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func health(w http.ResponseWriter, req *http.Request) {
@@ -18,7 +20,17 @@ func health(w http.ResponseWriter, req *http.Request) {
 	w.Write(jsonResp)
 }
 
+func hostname(w http.ResponseWriter, req *http.Request) {
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("<h1>VERSION 3: Hello from Health Checker %v</h1>", name)))
+}
+
 func main() {
 	http.HandleFunc("/health", health)
+	http.HandleFunc("/hostname", hostname)
 	http.ListenAndServe(":8000", nil)
 }
